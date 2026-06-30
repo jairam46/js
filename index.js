fabec48e3c5734a81aecfd05b0e045ecc21862b2
Jairam <guptajairam820@gmail.com>
2026-06-30 11:57:10 +0000

    s

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("62300 is a", (62 + 0.3333333333).toFixed(1) , typeof(62.3))
+// console.log("62300 is a", (62 + 0.3333333333).toFixed(1) , typeof(62.3))
+function jairam() {//block scope start
+    const a = 10;
+    // a=20
+    // a=30
+    console.log(a, 4)
+} //block scope end
+
+jairam();
+    const a = 25;
+    // a=45
+    console.log(a, 8)
